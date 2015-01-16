d4fa0c09a39954601be534bbe682b6b9df737d40
catlag <sammieeagle@gmail.com>
2015-01-15 19:18:54 -0800

    changed videos

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require("express"),
+  async = require("async"),
+  bodyParser = require("body-parser"),
+  passport = require("passport"),
+  passportLocal = require("passport-local"),
+  cookieParser = require("cookie-parser"),
+  session = require("cookie-session"),
+  db = require("./models/index"),
+  flash = require('connect-flash'),
+  methodOverride = require('method-override'),
+  request = require('request'),
+  app = express();
+ 
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
+
+app.get('/', function(req,res){
+  res.render('index');
+});
+
+
+
+
+
+app.get('*', function(req,res){
+  res.render('404');
+});
+
+
+app.listen(process.env.PORT || 3000, function(){
+  "Server is listening on port 3000. Yay!";
+});
